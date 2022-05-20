bd373478ee07472b88e4991ad600edb4cbf65ffc
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-20 23:46:52 +0530

    add new project

--- src/images/images.js
+++ src/images/images.js
@@ -3,10 +3,5 @@ import twitter_logo from "./twitter.png";
 import linkedin_logo from "./linkedin.png";
 import github_logo from "./github.png";
 import profile from "./PRAKHAR.jpg";
-import CPP from "./CPP.png"
-import PYTHON from "./PYTHON.png"
-import JAVA from "./JAVA.png"
-import REACT from "./REACT.png"
-import JAVASCRIPT from "./JAVASCRIPT.png"
 
-export {insta_logo,twitter_logo,linkedin_logo,github_logo,profile,JAVASCRIPT,REACT,PYTHON,CPP,JAVA};
+export { insta_logo, twitter_logo, linkedin_logo, github_logo, profile };
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./Card"
+import Card from "./Card";
 
 function Project() {
   let projects = [
@@ -10,10 +10,10 @@ function Project() {
       link: "https://github.com/prakhar-5447/Address-Book",
       host: "",
       // image: "https://raw.githubusercontent.com/prakhar-5447/firstmy/master/src/images/bg3.jpg",
-      tag: ["#PERSONAL","#WEB"],
+      tag: ["#PERSONAL", "#WEB"],
     },
     {
-      title: "DISCORD BOT",
+      title: "Discord Bot",
       desc: "Server Moderation bot help to make server secure and away from threats",
       tech: ["TYPESCRIPT"],
       link: "https://github.com/prakhar-5447/PROJECT-C",
@@ -22,19 +22,39 @@ function Project() {
       tag: ["#PERSONAL"],
     },
     {
+      title: "ToDo APP",
+      desc: "ToDo is a kind of web app that generally used to maintain our day-to-day tasks or list everything that we have to do.",
+      tech: ["HTML", "CSS", "TYPESCRIPT", "ANGULAR"],
+      link: "https://github.com/prakhar-5447/angular-project",
+      host: "",
+      // image: "https://raw.githubusercontent.com/prakhar-5447/firstmy/master/src/images/bg3.jpg",
+      tag: ["#WEB"],
+    },
+    {
       title: "Firstmy",
       desc: "FirstMy is dedicated to preventing child abuse by strengthening, equipping, and restoring children and families in crisis. One Life at a Time.",
       tech: ["HTML", "CSS", "JAVASCRIPT", "MERN"],
       link: "https://github.com/prakhar-5447/firstmy",
       host: "",
       // image: "https://raw.githubusercontent.com/prakhar-5447/firstmy/master/src/images/bg3.jpg",
-      tag: ["#TEAM","#WEB"],
-    }
-  ]
+      tag: ["#TEAM", "#WEB"],
+    },
+    {
+      title: "Juicy-N-Yummy",
+      desc: "Juicy-N-Yummy is an platform for restaurant aggregator and food delivery. It provides information, menus and user-reviews of restaurants.",
+      tech: ["HTML", "CSS", "TYPESCRIPT", "MEAN"],
+      link: "https://github.com/prakhar-5447/Juicy-N-Yummy",
+      host: "",
+      // image: "https://raw.githubusercontent.com/prakhar-5447/firstmy/master/src/images/bg3.jpg",
+      tag: ["#TEAM", "#WEB"],
+    },
+  ];
 
   return (
     <div className="container flex m-auto flex-wrap justify-evenly">
-      {projects.map(e => <Card key={e.title} project={e} />)}
+      {projects.map((e) => (
+        <Card key={e.title} project={e} />
+      ))}
     </div>
   );
 }
